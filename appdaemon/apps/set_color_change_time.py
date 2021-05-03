678c822c634d7e50992e4465942dda27cc409844
Freek Hoekstra <freekgeorgehoekstra@gmail.com>
2021-05-03 21:22:59 +0200

    [refactor] set color change time a bit

--- appdaemon/apps/set_color_change_time.py
+++ appdaemon/apps/set_color_change_time.py
@@ -1,42 +1,45 @@
 from datetime import datetime, date, timedelta, timezone
 from datetime import time
 from math import cos, pi
-from typing import Callable, Any
+from typing import Callable, Any, Iterable
 
 import hassapi as hass
 
 MIN_EVENING_TIME_UTC = time(hour=18)
 MAX_EVENING_TIME_UTC = time(hour=22)
 
+NIGHT_START_TIME = 'input_datetime.night_start_time'
+EVE_START_TIME = 'input_datetime.early_eve_start_time'
+ON_OFF_SWITCH = 'input_boolean.automatic_color_time_adjustment'
+INPUTS = ON_OFF_SWITCH, EVE_START_TIME, NIGHT_START_TIME
+
 SHORTEST_DAY_OF_MONTH = 21
 SHORTEST_DAY_MONTH = 12
-YEAR = timedelta(days=365)
-HOUR = timedelta(hours=1)
 
 
 # noinspection PyAttributeOutsideInit
 class ColorChangeTimer(hass.Hass):
 
     def initialize(self):
-        self.max_evening_time_utc = to_timedelta(MAX_EVENING_TIME_UTC)
-        self.min_evening_time_utc = to_timedelta(MIN_EVENING_TIME_UTC)
+        self.max_evening_time_utc = self.to_timedelta(MAX_EVENING_TIME_UTC)
+        self.min_evening_time_utc = self.to_timedelta(MIN_EVENING_TIME_UTC)
 
-        self.run_callback_on_change_of(
-            self.on_input_change,
-            (
-                'input_boolean.automatic_color_time_adjustment',
-                'input_datetime.early_eve_start_time',
-                'input_datetime.night_start_time',
-            ))
+        self.run_callback_on_change_of(self.on_input_change, INPUTS)
+        self.listen_event(self.on_event, event='homeassistant.start')  # TODO: test if this works
 
         self._my_run_every(
             callback=self.set_times,
             start=self.datetime() + timedelta(minutes=1),
             interval=timedelta(hours=2, minutes=7))
 
-    def run_callback_on_change_of(self, callback: Callable, entity_ids: list):
+    def run_callback_on_change_of(self, callback: Callable, entity_ids: Iterable[str]):
         for entity_id in entity_ids:
             self.listen_state(callback, entity_id)
+
+    def on_event(self, event_name: str, data: dict, kwargs: dict):
+        delay = timedelta(minutes=5)
+        self.log(f'Detected HA startup, scheduling run in {delay}')
+        self._my_run_in(self.set_times, delay)
 
     def on_input_change(self, entity, attribute, old, new, kwargs):
         self.set_times({})
@@ -49,7 +52,7 @@ class ColorChangeTimer(hass.Hass):
 
     def is_switched_on(self):
         switch_state = self.get_state(
-            entity_id='input_boolean.automatic_color_time_adjustment',
+            entity_id=ON_OFF_SWITCH,
             attribute='state')
         self.log(f'{switch_state=}')
         return switch_state == 'on'
@@ -60,7 +63,7 @@ class ColorChangeTimer(hass.Hass):
         formatted_time = self.evening_time.strftime('%H:%M:00')
         self.call_service(
             'input_datetime/set_datetime',
-            entity_id='input_datetime.early_eve_start_time',
+            entity_id=EVE_START_TIME,
             time=formatted_time)
         self.log(f'Set evening time to {formatted_time}')
         return self.evening_time
@@ -72,13 +75,13 @@ class ColorChangeTimer(hass.Hass):
         min_time_utc and max_time_utc time at which to change color on shortest
             and longest day, in utc
         """
-        time_since_solstice = now.date() - get_shortest_day_in(now.year)
+        time_since_solstice = now.date() - self.get_shortest_day_in(now.year)
 
         desired_time_in_utc: timedelta = \
             self.evening_average \
             + self.evening_amplitude * cos(2 * pi * time_since_solstice / YEAR)
 
-        evening_time_utc = to_midnight_utc(now) + desired_time_in_utc
+        evening_time_utc = self.to_midnight_utc(now) + desired_time_in_utc
         evening_time_local = evening_time_utc.astimezone(now.tzinfo)
         return evening_time_local
 
@@ -87,15 +90,18 @@ class ColorChangeTimer(hass.Hass):
         formatted_time = self.night_time.strftime('%H:%M:00')
         self.call_service(
             'input_datetime/set_datetime',
-            entity_id='input_datetime.night_start_time',
+            entity_id=NIGHT_START_TIME,
             time=formatted_time)
         self.log(f'Set night time to {formatted_time}')
 
     def get_night_time(self, evening_time: datetime) -> datetime:
         today = evening_time.date()
-        time_since_solstice: timedelta = today - get_shortest_day_in(today.year)
+        time_since_solstice: timedelta = today - self.get_shortest_day_in(today.year)
 
-        offset = 1.25*HOUR + 0.75*HOUR * cos(2*pi*time_since_solstice/YEAR)
+        hour = timedelta(hours=1)
+        offset = (1.25*hour
+                  + 0.75*hour * cos(
+                    2*pi * time_since_solstice / timedelta(days=365)))
         return self.evening_time + offset
 
     @property
@@ -114,19 +120,22 @@ class ColorChangeTimer(hass.Hass):
         self.run_every(callback, start=start,
                        interval=interval.total_seconds(), **kwargs)
 
+    def _my_run_in(self, callback: Callable[[dict], Any], delay: timedelta, **kwargs):
+        self.run_in(callback, delay, **kwargs)
 
-def to_midnight_utc(now: datetime) -> datetime:
-    return datetime(year=now.year, month=now.month, day=now.day,
-                    tzinfo=timezone.utc)
+    @staticmethod
+    def to_midnight_utc(now: datetime) -> datetime:
+        return datetime(year=now.year, month=now.month, day=now.day,
+                        tzinfo=timezone.utc)
 
+    @staticmethod
+    def get_shortest_day_in(year: int) -> date:
+        return date(year=year,
+                    month=SHORTEST_DAY_MONTH,
+                    day=SHORTEST_DAY_OF_MONTH)
 
-def get_shortest_day_in(year: int) -> date:
-    return date(year=year,
-                month=SHORTEST_DAY_MONTH,
-                day=SHORTEST_DAY_OF_MONTH)
-
-
-def to_timedelta(naive_time: time) -> timedelta:
-    return timedelta(hours=naive_time.hour,
-                     minutes=naive_time.minute,
-                     seconds=naive_time.second)
+    @staticmethod
+    def to_timedelta(naive_time: time) -> timedelta:
+        return timedelta(hours=naive_time.hour,
+                         minutes=naive_time.minute,
+                         seconds=naive_time.second)

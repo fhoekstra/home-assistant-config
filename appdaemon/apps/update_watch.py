88e0a4fbe035c6437cb4523dfc14ae5974ddf212
Freek Hoekstra <freekgeorgehoekstra@gmail.com>
2021-05-12 10:55:15 +0200

    [fix] typo in update watcher

--- appdaemon/apps/update_watch.py
+++ appdaemon/apps/update_watch.py
@@ -11,20 +11,13 @@ SYSTEMS_TO_WATCH = (
 class UpdateWatcher(hass.Hass):
     def initialize(self):
         self.listen_state(self.on_new_ha_version,
-                          entity='sensors.latest_ha_version')
+                          entity='sensor.latest_ha_version')
         for system in SYSTEMS_TO_WATCH:
             self.listen_state(
                 self.on_update_available,
                 entity=f'binary_sensor.{system}_update_available',
                 new='on',
             )
-
-    def on_new_ha_version(self, entity, attribute, old, new, kwargs):
-        self.call_service(
-            'notify.mobile_app_fp3',
-            message=f'Update available for HA Core.'
-                    f' Upgrade to {new} in Home Assistant'
-        )
 
     def on_update_available(self, entity, attribute, old, new, kwargs):
         system = entity[len('binary_sensor.')
@@ -36,4 +29,11 @@ class UpdateWatcher(hass.Hass):
             message=f'Update available for {system}.'
                     f' Upgrade from {current_version}'
                     f' to {new_version} in Home Assistant'
+        )
+
+    def on_new_ha_version(self, entity, attribute, old, new, kwargs):
+        self.call_service(
+            'notify.mobile_app_fp3',
+            message=f'Update available for HA Core.'
+                    f' Upgrade to {new} in Home Assistant'
         )
